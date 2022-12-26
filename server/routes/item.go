d1babf71036570ccd1bf15e57a9c2f40c27010b0
M. Mert Yildiran <me@mertyildiran.com>
2022-12-26 05:08:42 +0300

    :hammer: Move `GetItem` and `WebsocketHandler` into `controllers` package

--- server/routes/item.go
+++ server/routes/item.go
@@ -1,143 +1,15 @@
 package routes
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"strconv"
-	"strings"
-
 	"github.com/gin-gonic/gin"
-	"github.com/kubeshark/base/pkg/api"
-	"github.com/kubeshark/base/pkg/extensions"
-	"github.com/kubeshark/base/pkg/languages/kfl"
-	"github.com/kubeshark/worker/assemblers"
 	"github.com/kubeshark/worker/misc"
-	"github.com/kubeshark/worker/source"
-	"github.com/rs/zerolog/log"
+	"github.com/kubeshark/worker/server/controllers"
 )
 
 func ItemRoutes(ginApp *gin.Engine, opts *misc.Opts) {
 	routeGroup := ginApp.Group("/item")
 
 	routeGroup.GET("/:id", func(c *gin.Context) {
-		getItem(c, opts)
+		controllers.GetItem(c, opts)
 	})
-}
-
-func getItem(c *gin.Context, opts *misc.Opts) {
-	_id := c.Param("id")
-	idIndex := strings.Split(_id, "-")
-	if len(idIndex) < 2 {
-		msg := "Malformed ID!"
-		log.Error().Str("id", _id).Msg(msg)
-		misc.HandleError(c, fmt.Errorf(msg))
-		return
-	}
-	id := idIndex[0]
-	index, err := strconv.ParseInt(idIndex[1], 0, 64)
-	if err != nil {
-		log.Error().Err(err).Str("pcap", id).Str("index", idIndex[1]).Msg("Failed parsing index!")
-		misc.HandleError(c, err)
-		return
-	}
-
-	query := c.Query("q")
-	worker := c.Query("worker")
-
-	outputChannel := make(chan *api.OutputChannelItem)
-
-	streamsMap := assemblers.NewTcpStreamMap(false)
-	packets := make(chan source.TcpPacketInfo)
-	s, err := source.NewTcpPacketSource(id, misc.GetPcapPath(id), "", "libpcap")
-	if err != nil {
-		log.Error().Err(err).Str("pcap", id).Msg("Failed to create TCP packet source!")
-		misc.HandleError(c, err)
-		return
-	}
-	go s.ReadPackets(packets)
-
-	assembler := assemblers.NewTcpAssembler(id, false, outputChannel, streamsMap, opts)
-	go func() {
-		for {
-			packetInfo, ok := <-packets
-			if !ok {
-				break
-			}
-			assembler.ProcessPacket(packetInfo, false)
-		}
-	}()
-
-	var i int64 = -1
-	for item := range outputChannel {
-		i++
-		if i < index {
-			continue
-		}
-
-		// TODO: The previously bad design forces us to Marshal and Unmarshal
-		var data []byte
-		data, err = json.Marshal(item)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed marshalling item:")
-			break
-		}
-		var finalItem *api.OutputChannelItem
-		err = json.Unmarshal(data, &finalItem)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed unmarshalling item:")
-			misc.HandleError(c, err)
-			break
-		}
-
-		entry := itemToEntry(finalItem)
-		entry.Worker = worker
-		entry.Node = misc.RemovePortFromWorkerHost(worker)
-		entry.BuildId()
-		entry.Tls = misc.IsTls(entry.Stream)
-
-		protocol := extensions.ProtocolsMap[entry.Protocol.ToString()]
-		extension := extensions.ExtensionsMap[entry.Protocol.Name]
-
-		var entryMarshaled []byte
-		entryMarshaled, err = json.Marshal(entry)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed marshalling entry:")
-			break
-		}
-
-		var record string
-		_, record, err = kfl.Apply(entryMarshaled, query)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed applying query:")
-			break
-		}
-
-		var alteredEntry *api.Entry
-		err = json.Unmarshal([]byte(record), &alteredEntry)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed unmarshalling altered entry:")
-			break
-		}
-
-		base := extension.Dissector.Summarize(alteredEntry)
-		var representation []byte
-		representation, err = extension.Dissector.Represent(alteredEntry.Request, alteredEntry.Response)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed representing altered entry:")
-			break
-		}
-
-		entryWrapped := &api.EntryWrapper{
-			Protocol:       *protocol,
-			Representation: string(representation),
-			Data:           entry,
-			Base:           base,
-		}
-
-		c.JSON(http.StatusOK, entryWrapped)
-		return
-	}
-
-	misc.HandleError(c, err)
 }
