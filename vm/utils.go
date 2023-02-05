b828c9e7c6fbfe07a7996aec28ce72feaf9d8339
M. Mert Yildiran <me@mertyildiran.com>
2023-02-06 01:25:22 +0300

    :bug: Fix `ArgumentListToString` method

--- vm/utils.go
+++ vm/utils.go
@@ -4,8 +4,8 @@ import "github.com/robertkrimen/otto"
 
 func ArgumentListToString(args []otto.Value) (text string) {
 	for _, arg := range args {
-		if len(text) == 0 {
-			text += ""
+		if len(text) != 0 {
+			text += " "
 		}
 
 		text += arg.String()
