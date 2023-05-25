632f7061fe932e84cae73941ac93df5aa67d3550
Chi Su <chi@webb.ai>
2023-05-24 19:38:19 -0700

    do not use latest version

--- push-image.sh
+++ push-image.sh
@@ -2,7 +2,7 @@
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/p5v6t9h8
 export DOCKER_REPO="public.ecr.aws/p5v6t9h8/k8s-traffic-collector"
-export DOCKER_TAG="latest"
+export DOCKER_TAG=$(git describe --dirty --tags)
 docker build . -t ${DOCKER_REPO}:${DOCKER_TAG}-amd64 --build-arg BUILDARCH=amd64 --build-arg TARGETARCH=amd64
 docker build . -t ${DOCKER_REPO}:${DOCKER_TAG}-arm64v8 --build-arg BUILDARCH=amd64 --build-arg TARGETARCH=arm64v8
 
