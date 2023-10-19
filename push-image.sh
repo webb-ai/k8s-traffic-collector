a5fd406287ba50b8e302bdce0ad80a5c00751414
Chi Su <chi@webb.ai>
2023-10-18 20:22:17 -0700

    update push-image

--- push-image.sh
+++ push-image.sh
@@ -6,7 +6,7 @@ DOCKER_TAG=$(git describe --dirty --tags)
 docker build . -t ${DOCKER_REPO}:${DOCKER_TAG}-amd64 --build-arg BUILDARCH=amd64 --build-arg TARGETARCH=amd64 --push
 docker build . -t ${DOCKER_REPO}:${DOCKER_TAG}-arm64v8 --build-arg BUILDARCH=amd64 --build-arg TARGETARCH=arm64v8 --push
 
-
+sleep 10
 docker manifest create ${DOCKER_REPO}:${DOCKER_TAG} ${DOCKER_REPO}:${DOCKER_TAG}-amd64 ${DOCKER_REPO}:${DOCKER_TAG}-arm64v8
 docker manifest push ${DOCKER_REPO}:${DOCKER_TAG}
 
